52180fc728406e646d24c054d923613e57b74b7a
mic4ael <crossner90@gmail.com>
2013-10-27 22:28:40 +0100

    Added pagination to product groups.

--- src/main/java/pl/indecoders/archetype/repository/product/ProductRepository.java
+++ src/main/java/pl/indecoders/archetype/repository/product/ProductRepository.java
@@ -2,6 +2,7 @@ package pl.indecoders.archetype.repository.product;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
