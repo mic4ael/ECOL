743369527fab7d319046205896cf342bf3a772e7
mic4ael <crossner90@gmail.com>
2013-11-11 12:17:36 +0100

    m

--- src/main/java/pl/indecoders/archetype/validation/annotation/UniqueProductName.java
+++ src/main/java/pl/indecoders/archetype/validation/annotation/UniqueProductName.java
@@ -1,5 +1,25 @@
 package pl.indecoders.archetype.validation.annotation;
 
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+import pl.indecoders.archetype.validation.engine.UniqueProductNameValidator;
+
+@Target({ FIELD, ANNOTATION_TYPE })
+@Retention(RUNTIME)
+@Constraint(validatedBy = UniqueProductNameValidator.class)
+@Documented
 public @interface UniqueProductName {
 
+	String message() default "{unique.productName}";
+	Class<?>[] groups() default {};
+	Class<? extends Payload>[] payload() default {};
 }
